26e5089c1a2107223fa7602d08fbc7b354a3221b
jmdevelopr <57146955+jmdevelopr@users.noreply.github.com>
2020-05-15 18:29:00 +0200

    Update fbConfig.js

--- src/fbConfig.js
+++ src/fbConfig.js
@@ -3,13 +3,7 @@ import 'firebase/firestore';
 
 // Your web app's Firebase configuration
   var firebaseConfig = {
-    apiKey: "AIzaSyCCIxX0lg-9SEaXWoFiTtIHk705a0G6dxg",
-    authDomain: "typereact.firebaseapp.com",
-    databaseURL: "https://typereact.firebaseio.com",
-    projectId: "typereact",
-    storageBucket: "typereact.appspot.com",
-    messagingSenderId: "24113643493",
-    appId: "1:24113643493:web:c5651f202ea8010a90b90c"
+    //here would be my firebase informations that I want to keep private for safety reasons
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
